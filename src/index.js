fd1d56cbe7d857f671746cb1ba078ca0f79994e4
Jeremy <jeremy.ling@unotech.io>
2021-07-29 11:28:34 +0800

    fix export

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-import FormBuilder from "./Components/FormBuilder";
-import StandardAutocomplete from "./Components/Forms/StandardAutocomplete";
-import StandardDatePicker from "./Components/Forms/StandardDatePicker";
-import StandardDateTimePicker from "./Components/Forms/StandardDateTimePicker";
-import StandardSelect from "./Components/Forms/StandardSelect";
-import StandardTextField from "./Components/Forms/StandardTextField";
+import HorizontalScrollList from "./Components/HorizontalScrollList";
 
-export {
-  StandardAutocomplete,
-  StandardDatePicker,
-  StandardDateTimePicker,
-  StandardSelect,
-  StandardTextField,
-};
-export default FormBuilder;
+export default HorizontalScrollList;
